525d7990d30594f7fcdb63cf93a676bfb1caea8c
camiledealano <alanocamile@gmail.com>
2023-06-19 21:26:56 -0300

    Inicio do trabalho, criação da tela, textArea e botão para fins de teste

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import array.TelaArray;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaArray telaArray = new TelaArray();    
+    }
+}
---
+++ src/array/TelaArray.java
@@ -0,0 +1,33 @@
+package array;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class TelaArray extends JFrame {
+
+    private JTextArea textArea;
+    private JButton btnAZ;
+
+    public TelaArray() {
+        setSize(700, 500);
+        setTitle("Tela de Array");
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        criarComponentes();
+        setVisible(true);
+    }
+
+    private void criarComponentes() {
+        textArea = new JTextArea();
+        textArea.setBounds(10, 10, 350, 440);
+        getContentPane().add(textArea);
+
+        btnAZ = new JButton();
+        btnAZ.setBounds(380, 10, 80, 40);
+        getContentPane().add(btnAZ);
+
+    }
+
+    
+}
